5d45b44b71529443ff6761e6de1c20df14fd2ba2
hoihoi0101 <114476214+hoihoi0101@users.noreply.github.com>
2023-05-27 22:18:33 +0900

    map update

--- src/js/map.js
+++ src/js/map.js
@@ -1,11 +1,11 @@
-var apiKey = "apikey";
+var apiKey = "AIzaSyCFnSas3ZXz4SFIIs1vT2lbu5iOQOuQqDQ";
 var myPosition = { lat: 36.625, lng: 127.454 };
 var movies = [
-    { name: "메가박스", address: "충청북도 청주시 서원구 1순환로 682 메가박스 청주사창점", url: "megabox.png", info: "1544-0070", lat: 36.632, lng: 127.460 },
-    { name: "CGV 청주터미널", address: "충청북도 청주시 흥덕구 가경동 1416-3", url: "cgv_terminal.png", info: "1544-1122", lat: 36.626, lng: 127.431 },
-    { name: "롯데시네마 서청주", address: "충청북도 청주시 흥덕구 비하동 순환로 1004", url: "lottecinema_cheongju.png", info: "1544-8855", lat: 36.644, lng: 127.420 },
-    { name: "CGV 청주", address: "충청북도 청주시 상당구 상당로81번길 63", url: "cgv_cheongju.png", info: "1544-1122", lat: 36.635, lng: 127.486 },
-    { name: "CGV 청주성안길", address: "충청북도 청주시 상당구 상당로81번길 33", url: "cgv_cheongju_load.png", info: "1544-1122", lat: 36.635, lng: 127.488 } ];
+    { name: "메가박스", address: "충청북도 청주시 서원구 1순환로 682 메가박스 청주사창점", url: "../../img/megabox.png", info: "1544-0070", lat: 36.632, lng: 127.460 },
+    { name: "CGV 청주터미널", address: "충청북도 청주시 흥덕구 가경동 1416-3", url: "../../img/cgv_terminal.png", info: "1544-1122", lat: 36.626, lng: 127.431 },
+    { name: "롯데시네마 서청주", address: "충청북도 청주시 흥덕구 비하동 순환로 1004", url: "../../img/lottecinema_cheongju.png", info: "1544-8855", lat: 36.644, lng: 127.420 },
+    { name: "CGV 청주", address: "충청북도 청주시 상당구 상당로81번길 63", url: "../../img/cgv_cheongju.png", info: "1544-1122", lat: 36.635, lng: 127.486 },
+    { name: "CGV 청주성안길", address: "충청북도 청주시 상당구 상당로81번길 33", url: "../../img/cgv_cheongju_load.png", info: "1544-1122", lat: 36.635, lng: 127.488 } ];
 
 // window.onload 이벤트에 html창 열릴 시 실행할 함수 연결
 window.onload = initMap;
@@ -46,9 +46,9 @@ function showMap() {
             content: `<img src=${url} alt="" width="120px" height="120px"><br>
             <strong style="font-size: 15px">${name}</strong><br>
             <span style="font-size:10px; width:120px; text-overflow: ellipsis; white-space:nowrap; overflow:hidden; display:block;">
-            <img src="marker.png" alt="" width="10px height="10px">${address}</span>
+            <img src="../../img/marker.png" alt="" width="10px height="10px">${address}</span>
             <span style="font-size:10px; width:120px; text-overflow: ellipsis; white-space:nowrap; overflow:hidden; display:block;">
-            <img src="call.png" alt="" width="10px height="10px">${info}</span>`
+            <img src="../../img/call.png" alt="" width="10px height="10px">${info}</span>`
         });
         // 마커 hover 이벤트 처리
         marker.addListener('mouseover', function() {
