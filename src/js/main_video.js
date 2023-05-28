07e4a086c65f83cf8966a0c7487bede9138e8d68
seungmin <lsm990429@gmail.com>
2023-05-28 19:29:14 +0900

    상세정보 데이터 추가

--- src/js/main_video.js
+++ src/js/main_video.js
@@ -1,42 +1,10 @@
-var gifContainer1 = document.querySelector('.slideitem1'); //슬라이드 1번 객체화
-var gifContainer2 = document.querySelector('.slideitem2');  //슬라이드 2번 객체화
-var gifContainer3 = document.querySelector('.slideitem3');  //슬라이드 3번 객체화
-var gifContainer4 = document.querySelector('.slideitem4');  //슬라이드 4번 객체화
-
+var gifContainer = document.querySelector('.slideitem1');
     var gifImage = document.getElementById('myGif');
 
-    gifContainer1.addEventListener('mouseenter', function() {
-      gifImage.src = "../../video/galxy.gif"
+    gifContainer.addEventListener('mouseenter', function() {
       gifImage.style.display = 'block';
     });
 
-    gifContainer1.addEventListener('mouseleave', function() {
-      gifImage.style.display = 'none';
-    });
-
-    gifContainer2.addEventListener('mouseenter', function() {
-      gifImage.src = "../../video/city.gif"
-      gifImage.style.display = 'block';
-    });
-
-    gifContainer2.addEventListener('mouseleave', function() {
-      gifImage.style.display = 'none';
-    });
-
-    gifContainer3.addEventListener('mouseenter', function() {
-      gifImage.src = "../../video/mission.gif"
-      gifImage.style.display = 'block';
-    });
-
-    gifContainer3.addEventListener('mouseleave', function() {
-      gifImage.style.display = 'none';
-    });
-
-    gifContainer4.addEventListener('mouseenter', function() {
-      gifImage.src = "../../video/speed.gif"
-      gifImage.style.display = 'block';
-    });
-
-    gifContainer4.addEventListener('mouseleave', function() {
+    gifContainer.addEventListener('mouseleave', function() {
       gifImage.style.display = 'none';
     });
