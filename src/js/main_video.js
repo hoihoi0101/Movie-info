72a5772f160afc826edf522bb9bfa5b1593f1d2a
hoihoi0101 <114476214+hoihoi0101@users.noreply.github.com>
2023-05-28 18:04:39 +0900

    Visual_update

--- src/js/main_video.js
+++ src/js/main_video.js
@@ -1,10 +1,42 @@
-var gifContainer = document.querySelector('.slideitem1');
+var gifContainer1 = document.querySelector('.slideitem1');
+var gifContainer2 = document.querySelector('.slideitem2');
+var gifContainer3 = document.querySelector('.slideitem3');
+var gifContainer4 = document.querySelector('.slideitem4');
+
     var gifImage = document.getElementById('myGif');
 
-    gifContainer.addEventListener('mouseenter', function() {
+    gifContainer1.addEventListener('mouseenter', function() {
+      gifImage.src = "../../video/galxy.gif"
       gifImage.style.display = 'block';
     });
 
-    gifContainer.addEventListener('mouseleave', function() {
+    gifContainer1.addEventListener('mouseleave', function() {
+      gifImage.style.display = 'none';
+    });
+
+    gifContainer2.addEventListener('mouseenter', function() {
+      gifImage.src = "../../video/galxy.gif"
+      gifImage.style.display = 'block';
+    });
+
+    gifContainer2.addEventListener('mouseleave', function() {
+      gifImage.style.display = 'none';
+    });
+
+    gifContainer3.addEventListener('mouseenter', function() {
+      gifImage.src = "../../video/galxy.gif"
+      gifImage.style.display = 'block';
+    });
+
+    gifContainer3.addEventListener('mouseleave', function() {
+      gifImage.style.display = 'none';
+    });
+
+    gifContainer4.addEventListener('mouseenter', function() {
+      gifImage.src = "../../video/galxy.gif"
+      gifImage.style.display = 'block';
+    });
+
+    gifContainer4.addEventListener('mouseleave', function() {
       gifImage.style.display = 'none';
     });
